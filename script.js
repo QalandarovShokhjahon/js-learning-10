7224479ed6678c93ffe7040cdb37ca11f01eec9e
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-25 19:11:43 +0500

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Callback → bu boshqa funksiya ichida chaqiriladigan funksiya.
+
+// setTimeout() → bu funksiyaga berilgan kodni belgilangan vaqt (millisekundlarda) o‘tgach faqat bir marta bajarish uchun ishlatiladi.
+
+setTimeout(() => {
+  console.log("TechWebby Tutorials");
+}, 1000)
