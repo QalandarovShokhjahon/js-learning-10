0f5a4a94541eb4b8342a1011af336c4e98827a83
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-26 19:51:42 +0500

    initial

--- script.js
+++ script.js
@@ -4,6 +4,18 @@
 
 // setTimeout() → bu funksiyaga berilgan kodni belgilangan vaqt (millisekundlarda) o‘tgach faqat bir marta bajarish uchun ishlatiladi.
 
-setTimeout(() => {
-  console.log("TechWebby Tutorials");
-}, 1000)
+// setTimeout(() => {
+//   console.log("TechWebby");
+// }, 1000);
+
+// Vazifa:
+let firstlog = 'firstLog()';
+let secondlog = 'secondLog()';
+
+setTimeout((firstLog) => {
+  console.log(firstLog);
+
+}, 2000, firstlog);
+console.log(secondlog);
+
+
